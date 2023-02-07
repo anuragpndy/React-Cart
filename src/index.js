6831761f0759f8f44654b651333d6658911883cd
Anurag Pandey <anurag.pndy@gmail.com>
2023-02-07 15:57:19 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from "react";
+// import ReactDOM from "react-dom";
+import React  from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+// import * as firebase from "firebase";
+// import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const firebaseConfig = {
+  apiKey: "AIzaSyCA1anZObGwoqYDYz6DTHmUhb0Vhslp6og",
+  authDomain: "cart-61f40.firebaseapp.com",
+  projectId: "cart-61f40",
+  storageBucket: "cart-61f40.appspot.com",
+  messagingSenderId: "997425510110",
+  appId: "1:997425510110:web:ad2c9b1f8c8805622db44f"
+};
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+
+export default firebase;
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
